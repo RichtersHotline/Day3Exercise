c064fcf762acb67dcd8970ffcf8668af90355f17
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-05-17 17:50:44 +0200

    examples

---
+++ Script/code.js
@@ -0,0 +1,19 @@
+//String
+let phrase = "Hotline";
+console.log(typeof phrase) 
+//Number
+let Start = 1981;
+console.log(typeof Start) 
+// Booleans
+let Ringing = true;
+console.log(typeof Ringing) 
+
+//Object 
+
+const person = {firstName: "Jacket", lastName: "Miami"}
+console.log(typeof person) 
+
+//Array
+
+let tools = ["Hammer", "Knife", "Katana", "Drill"];
+console.log(typeof tools) 
